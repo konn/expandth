8fe6d517d016e7450a84c2b045a5d1b8f33bc996
Hiromi Ishii <konn.jinro@gmail.com>
2015-08-23 02:46:14 +0900

    removed unused imports

--- examples/simple-expanded.hs
+++ examples/simple-expanded.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 import Language.Haskell.TH
-import Language.Haskell.TH.Traced
  
 hello_0 :: GHC.Base.String
 hello_0 = "hay!"
--- examples/simple.hs
+++ examples/simple.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 import Language.Haskell.TH
-import Language.Haskell.TH.Traced
 
 do fun <- newName "hello"
    sequence [sigD fun [t|String|], funD fun [clause [] (normalB [|"hay!"|]) []]]
