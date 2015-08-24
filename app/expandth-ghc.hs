8d4eecdf1bbe0471755a6b57dee5e8d49987ccb1
Hiromi Ishii <konn.jinro@gmail.com>
2015-08-25 02:58:21 +0900

    Module cleanup

--- app/expandth-ghc.hs
+++ app/expandth-ghc.hs
@@ -1,28 +1,39 @@
-{-# LANGUAGE DataKinds, OverloadedStrings, QuasiQuotes, RecordWildCards #-}
-{-# LANGUAGE TemplateHaskell                                            #-}
+{-# LANGUAGE DataKinds, ExtendedDefaultRules, OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes, RecordWildCards, TemplateHaskell      #-}
 module Main where
 import Language.Haskell.TH.Expand
 
 import           Control.Monad                (forM_)
+import           Control.Monad                (when)
 import           Control.Monad.Trans          (liftIO)
 import           Data.Monoid                  ((<>))
+import           Data.Proxy                   (Proxy (..))
 import           Data.String                  (fromString)
 import qualified Data.Text                    as T
 import           Language.Haskell.Exts        (Decl (..), ImportDecl (..))
 import           Language.Haskell.Exts        (Module (..), ModuleName (..))
 import           Language.Haskell.Exts        (ParseResult (ParseOk), parseFile)
-import           Language.Haskell.Exts        (prettyPrint)
+import           Language.Haskell.Exts        (ParseMode (..), prettyPrint)
+import           Language.Haskell.Exts        (KnownExtension (PackageImports))
+import           Language.Haskell.Exts        (defaultParseMode)
+import           Language.Haskell.Exts        (fromParseResult,
+                                               parseModuleWithMode)
+import           Language.Haskell.Exts        (Extension (EnableExtension))
 import           Language.Haskell.Exts.QQ     (hs)
 import           Language.Haskell.Exts.SrcLoc (noLoc)
 import           Options.Declarative          (Arg, Cmd, Def, Flag, get)
 import qualified Options.Declarative          as Opt
 import           Shelly                       (canonic, cp, fromText, ls, pwd)
-import           Shelly                       (run_, shelly, silently)
+import           Shelly                       (run_, shelly, silently, test_e)
 import           Shelly                       (toTextIgnore, withTmpDir)
-import           Shelly                       (echo, mv, readfile, writefile,
-                                               (</>))
-import           System.FilePath              (takeDirectory, takeFileName)
+import           Shelly                       (cd, echo, echo_err, mv, readfile,
+                                               writefile, (</>))
+import           Shelly                       (rm)
+import           System.FilePath              (takeBaseName, takeDirectory,
+                                               takeFileName)
 import           System.Random                (randomIO)
+
+default (String)
 
 
 main :: IO ()
@@ -57,12 +68,14 @@ doMain ghcCmd opts moutc target = liftIO $ do
     cwd <- pwd
     origPath <- takeDirectory . T.unpack . toTextIgnore <$> canonic (fromString $ get target)
     withTmpDir $ \dir -> do
+      cd dir
       nonce <- liftIO randomIO
-      let Module loc n ps mws mes is ds = traceTHSplices m
+      let Module loc n@(ModuleName mn) ps mws mes is ds = traceTHSplices m
           is' = map (\n -> ImportDecl noLoc (ModuleName n) True False False Nothing Nothing Nothing)
                 ["Language.Haskell.Exts", "GHC.Base", "GHC.Types", "Language.Haskell.TH"
                 ,"Language.Haskell.Exts.Annotated.Syntax", "System.IO"]
-          tmpout = dir </> ("M" ++ (show (abs nonce + 1 :: Int)) ++ ".hs" :: String)
+          tbmname = "M" ++ show (abs nonce + 1 :: Int)
+          tmpout = dir </> (tbmname ++ ".hs" :: String)
           addFin = [hs|do
             Language.Haskell.TH.Syntax.addModFinalizer $(finalize (T.unpack $ toTextIgnore tmpout) m)
             return []|]
@@ -73,9 +86,19 @@ doMain ghcCmd opts moutc target = liftIO $ do
           searchPath = "-i" <> toTextIgnore dir <> ":" <> toTextIgnore cwd
       writefile dest $ T.pack $ prettyPrint modified
       run_ (fromText $ T.pack ghc) (map T.pack (words args)
-                                    ++ ["-i" , "" , searchPath, "-dynamic", "-c", toTextIgnore dest])
+                                    ++ [searchPath, "-dynamic", "-c", toTextIgnore dest])
+      run_ (fromText $ T.pack ghc) (map T.pack (words args)
+                                    ++ [searchPath, "-dynamic", "-ddump-minimal-imports", "-c", toTextIgnore tmpout])
+      let imps = dir </> (mn ++ ".imports")
+      imps_ok <- test_e imps
+      when imps_ok $ do
+        il <- readfile imps
+        let ParseOk (Module _ _ _ _ _ idecls _) = parseModuleWithMode
+                             defaultParseMode { extensions = [EnableExtension PackageImports] } $ T.unpack il
+        ParseOk (Module i p c f d _ u) <- liftIO $ parseFile $ T.unpack $ toTextIgnore tmpout
+        writefile tmpout $ T.pack $ prettyPrint $ Module i p c f d idecls u
       chs <- ls dir
-      forM_ (filter (`notElem` [dest, tmpout]) chs) $ \f -> cp f (fromString origPath)
+      forM_ (filter (\a -> a `notElem` [dest, tmpout, imps] && takeBaseName (T.unpack $ toTextIgnore a) == tbmname) chs) $ \f -> cp f (fromString origPath)
       case moup of
         Nothing -> echo =<< readfile tmpout
         Just fp -> mv tmpout (fromText $ T.pack fp)
